271d648ef9814dc2ffbbea86096c5a6225aa3207
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-08-29 23:41:21 -0300

    Render whitespace

--- src/whitespace.js
+++ src/whitespace.js
@@ -17,5 +17,54 @@ export default class Whitespace {
         }
 
         return this._getNewlinesBetween(startToken, endToken);
-      }
+    }
+
+    getNewlinesAfter(node) {
+        let startToken;
+        let endToken;
+        const tokens = this.tokens;
+
+        let index = this._findToken((token) => token.end - node.end, 0, tokens.length);
+        if (index >= 0) {
+            while (index && node.end === tokens[index - 1].end) --index;
+            startToken = tokens[index];
+            endToken = tokens[index + 1];
+            if (endToken.type.label === ",") endToken = tokens[index + 2];
+        }
+
+        return endToken && endToken.type.label === "eof"
+            ? 1
+            : this._getNewlinesBetween(startToken, endToken);
+    }
+
+    _getNewlinesBetween(startToken, endToken) {
+        if (!endToken || !endToken.loc) return 0;
+
+        const start = startToken ? startToken.loc.end.line : 1;
+        const end = endToken.loc.start.line;
+        let lines = 0;
+
+        for (let line = start; line < end; line++) {
+            if (typeof this.used[line] === "undefined") {
+                this.used[line] = true;
+                lines++;
+            }
+        }
+
+        return lines;
+    }
+
+    _findToken(test, start, end) {
+        if (start >= end) return -1;
+        const middle = (start + end) >>> 1;
+        const match = test(this.tokens[middle]);
+        if (match < 0) {
+            return this._findToken(test, middle + 1, end);
+        } else if (match > 0) {
+            return this._findToken(test, start, middle);
+        } else if (match === 0) {
+            return middle;
+        }
+        return -1;
+    }
 }
